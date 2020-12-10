b8716492cc689a7bfc97221ec9ddb6ea21e33dc7
petert360 <peter.t360@hotmail.com>
2020-12-10 21:49:50 +0100

    js update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+
+// dátum kezelés változói
 const todayElement = document.querySelector('.date__dayName');
 const dateElement = document.querySelector('.date__numericDate');
+
+// input mező és input gomb elemek kiválasztása és a gombra kettintás figyelése
+const buttonElement = document.querySelector('.add__button');
+const inputElement = document.querySelector('.add__input');
+buttonElement.addEventListener('click', clickHandler);
 
 const dateHandler = {
     getCurrentDay: function () {
@@ -21,3 +28,17 @@ const dateHandler = {
 console.log(dateHandler.getCurrentDate());
 todayElement.innerHTML = dateHandler.getCurrentDay();
 dateElement.innerHTML = dateHandler.getCurrentDate();
+
+
+// gomb lenyomására az input mező tartamát kiírjuk a console-ra
+
+function clickHandler() {
+    input = inputElement.value;
+    // ha üres a beviteli mező, nem történik semmi
+    if (!input) return; 
+    //inputElement
+    inputElement.value = '';
+    console.log(input);
+}
+
+
